27d9f7b3a90fa694c0ae34c0845b2de20c727cef
yahya <yahyaboukhmira7@gmail.com>
2025-05-14 20:48:57 +0200

    progress_in_bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 21:05:20 by marvin            #+#    #+#             */
+/*   Updated: 2025/05/12 21:05:20 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	allocate_buff(int fd, char **buff)
+{
+	size_t	fdlen;
+
+	*buff = (char *)malloc(BUFFER_SIZE + 1);
+	fdlen = read(fd, *buff, BUFFER_SIZE);
+	if (fdlen <= 0)
+	{
+		free(*buff);
+		*buff = NULL;
+		return (0);
+	}
+	(*buff)[fdlen] = '\0';
+	return (fdlen);
+}
+
+char	*create_line(char *buff)
+{
+	char	*line;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (buff[i] != '\0' && buff[i] != '\n')
+		i++;
+	line = (char *)malloc(i + 2);
+	if (!line)
+		return (NULL);
+	j = 0;
+	while (i > j)
+	{
+		line[j] = buff[j];
+		j++;
+	}
+	if (buff[j] == '\n')
+		line[j] = '\n';
+	j++;
+	line[j] = '\0';
+	return (line);
+}
+
+void	re_adjust_buff(char **buff)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while ((*buff)[i] != '\0' && (*buff)[i] != '\n')
+		i++;
+	if ((*buff)[i] == '\n')
+		i++;
+	j = 0;
+	while ((*buff)[i] != '\0')
+	{
+		(*buff)[j] = (*buff)[i];
+		i++;
+		j++;
+	}
+	(*buff)[j] = '\0';
+}
+
+char	*get_next_line(int fd)
+{
+	static char		*buff;
+	char			*line;
+
+	if (fd < 0)
+		return (NULL);
+	if (!buff)
+	{
+		if (allocate_buff(fd, &buff) == 0)
+			return (NULL);
+	}
+	line = create_line(buff);
+	re_adjust_buff(&buff);
+	if (buff[0] == '\0')
+	{
+		free(buff);
+		buff = NULL;
+	}
+	return (line);
+}
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <fcntl.h>
 # include <stdio.h>
@@ -20,6 +20,10 @@
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
+# endif
+
+# ifndef FD_SIZE
+#  define FD_SIZE 4096
 # endif
 
 char	*get_next_line(int fd);
